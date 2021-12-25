63fbb8a5c647bd725332ee206b4588870cc24933
hans <hans.krebs@me.com>
2021-12-25 12:18:43 +0100

    Implement MVP.

---
+++ src/commandCodeLensProvider.ts
@@ -0,0 +1,62 @@
+import vscode = require('vscode');
+
+export class CommandCodeLensProvider implements vscode.CodeLensProvider {
+  onDidChangeCodeLenses?: vscode.Event<void>;
+
+  provideCodeLenses(
+    document: vscode.TextDocument,
+    token: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.CodeLens[]> {
+    var codeLenses = [];
+    const lines = document.getText().split('\n');
+
+    var inCommand = false;
+    var currentCommand = '';
+    var commandStartLine = 0;
+    for (var i = 0; i < lines.length; i++) {
+      const line = lines[i].trim();
+      if (inCommand) {
+        if (line === '```') {
+          const cmd: vscode.Command = {
+            title: 'Execute command in terminal',
+            command: 'markdown-execute.execute',
+            arguments: [{ command: currentCommand }],
+          };
+          codeLenses.push(
+            new vscode.CodeLens(
+              new vscode.Range(
+                new vscode.Position(commandStartLine, 0),
+                new vscode.Position(commandStartLine + 1, 0)
+              ),
+              cmd
+            )
+          );
+          inCommand = false;
+          currentCommand = '';
+          continue;
+        }
+
+        currentCommand += line + '\n';
+        continue;
+      }
+
+      if (
+        line.startsWith('```') ||
+        line.startsWith('```sh') ||
+        line.startsWith('```bash')
+      ) {
+        inCommand = true;
+        commandStartLine = i;
+        continue;
+      }
+    }
+    return codeLenses;
+  }
+
+  resolveCodeLens?(
+    codeLens: vscode.CodeLens,
+    token: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.CodeLens> {
+    return null;
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,82 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+// promisify cp.exec
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+import { CommandCodeLensProvider } from './commandCodeLensProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "makrdown-execute" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('makrdown-execute.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from makrdown-execute!');
-	});
-
-	context.subscriptions.push(disposable);
+export async function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      'markdown-execute.execute',
+      async (args) => {
+        vscode.window.showInformationMessage('Sent to terminal for execution!');
+        // get the extension config
+        const { allowSsh } =
+          vscode.workspace.getConfiguration('markdown-execute');
+        // get active terminal or create new one
+        const term =
+          vscode.window.activeTerminal || vscode.window.createTerminal();
+        // get the process id of the terminal
+        const pid = await term.processId;
+        // get the state of the terminal
+        const { stdout: stateStdout, stderr: stateStderr } = await exec(
+          `ps -o state= -p ${pid}`
+        );
+        if (stateStderr) {
+          // if we can't check just send to the current one...
+          term.show();
+          term.sendText(args.command);
+          return;
+        }
+        // check if there's a running command in the active terminal
+        if (stateStdout?.includes('+')) {
+          // a + in the state indicates a process running in foreground
+          // so our terminal is not busy
+          // hence we can send the command to the terminal
+          term.show();
+          term.sendText(args.command);
+          return;
+        }
+        // if we're here, the terminal is busy
+        // to check if the the foreground process is ssh,
+        // we need to get the terminal's child process first
+        const { stdout: childProcessesStdout } = await exec(`pgrep -P ${pid}`);
+        if (childProcessesStdout && !isNaN(childProcessesStdout)) {
+          // then, get the command of the child process
+          const { stdout: childProcessCmdStdout } = await exec(
+            `ps -o command= -p ${childProcessesStdout}`
+          );
+          if (childProcessCmdStdout?.includes('ssh') && allowSsh) {
+            // if the child process is ssh and user allowed
+            // execution in ssh sessions
+            // we can send the command to it
+            term.show();
+            term.sendText(args.command);
+            return;
+          }
+        }
+        // if we're here, the terminal is busy with a non-ssh process
+        // or user doesn't allow ssh execution
+        // create a new term
+        const newTerm = vscode.window.createTerminal();
+        // execute the command in the new terminal
+        newTerm.show();
+        newTerm.sendText(args.command);
+        return;
+      }
+    )
+  );
+  // Add the command to markdown files
+  context.subscriptions.push(
+    vscode.languages.registerCodeLensProvider(
+      { language: 'markdown', scheme: 'file' },
+      new CommandCodeLensProvider()
+    )
+  );
 }
 
 // this method is called when your extension is deactivated
